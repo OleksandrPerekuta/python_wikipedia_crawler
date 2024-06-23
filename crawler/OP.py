1951257699749eb04acaf83b73b4207de634b3f4
Oleksandr Perekuta <perekuta.a@gmail.com>
2024-06-23 16:38:45 +0200

    changed parsing logic

--- crawler/OP.py
+++ crawler/OP.py
@@ -1,19 +1,53 @@
 import urllib.request
 import ssl
 import re
+from bs4 import BeautifulSoup
+from collections import defaultdict
 
 
-def find_links(text):
-    url_regex = r'https?://\S+'
-    urls = re.findall(url_regex, text)
-    #remove "> from links
-    cleaned_urls = [url.rstrip('">') for url in urls]
-    #todo: optimisation (not include .jpg, .png and others)
-    return cleaned_urls
+class Parser:
+    @staticmethod
+    def get_words_with_links(url):
+        context = ssl._create_unverified_context()
+        html_contents_of_page = urllib.request.urlopen(url, context=context).read().decode('utf-8')
+        soup = BeautifulSoup(html_contents_of_page, 'html.parser')
 
-page="https://en.wikipedia.org/wiki/Peter_Higgs"
-context = ssl._create_unverified_context()
-html_contents_of_page=urllib.request.urlopen(page, context=context)
-html_contents_of_page=html_contents_of_page.read().decode('utf-8')
-list_of_links=find_links(html_contents_of_page)
-print(list_of_links)
+        words_with_links = {}
+
+        body_content = soup.find('div', id='bodyContent')
+        if body_content:
+            for a_tag in body_content.find_all('a', href=True):
+                link = a_tag['href']
+                if link.startswith('/wiki/'):
+                    word = a_tag.get_text()
+                    clean_word = re.sub(r'\W+', '', word).lower()
+                    if clean_word:
+                        words_with_links[clean_word] = link
+
+        return words_with_links
+
+    @staticmethod
+    def get_all_words(url):
+        context = ssl._create_unverified_context()
+        html_contents_of_page = urllib.request.urlopen(url, context=context).read().decode('utf-8')
+        soup = BeautifulSoup(html_contents_of_page, 'html.parser')
+
+        word_count = defaultdict(int)
+
+        body_content = soup.find('div', id='bodyContent')
+        if body_content:
+            for text in body_content.stripped_strings:
+                words = re.findall(r'\b\w+\b', text.lower())
+                for word in words:
+                    word_count[word] += 1
+
+        return dict(word_count)
+
+
+url = "https://en.wikipedia.org/wiki/Peter_Higgs"
+words_with_links = Parser.get_words_with_links(url)
+all_words = Parser.get_all_words(url)
+
+print("Words with Links:", words_with_links)
+# мапа слова и количества появлений
+print("All Words:", all_words)
